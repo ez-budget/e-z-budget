c4c24e1976a6c2eff30dd4385332bae19cabafd9
franctech <32376285+rosefrancis-tech@users.noreply.github.com>
2021-03-20 12:32:46 -0400

    comment links to non-working files

--- controllers/index.js
+++ controllers/index.js
@@ -5,7 +5,9 @@ const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
 
 router.use('/', homeRoutes);
-router.use('/dashboard', dashboardRoutes);
+//router.use('/dashboard', dashboardRoutes);
+//router.use('/landing', dashboardRoutes);
 router.use('/api', apiRoutes);
+
 
 module.exports = router;
