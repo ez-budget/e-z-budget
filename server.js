a3525fcad4bd081a20a7967cdb04d512c71de736
Mateo <mtaycruz@gmail.com>
2021-03-19 22:59:25 -0400

    divs now have spacing between them

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // app.use(require('./controllers/'));
 
 app.get('/', (req, res) => {
-  res.render('landingpage');
+  res.render('homepage');
 })
 
 app.listen(3000, () => {
