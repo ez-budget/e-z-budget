565d73100f666d53f6520c830c9b0ab6ee685ee8
franctech <32376285+rosefrancis-tech@users.noreply.github.com>
2021-03-22 19:57:30 -0400

    show budget links on dashboards

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes.js');
 const postRoutes = require('./story-routes');
 const commentRoutes = require('./comment-routes');
+const budgetRoutes = require('./budget-routes');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
+router.use('/budgets', budgetRoutes);
 
 module.exports = router;
--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,33 +1,52 @@
 const router = require('express').Router();
-const { Post, User, Comment } = require('../models');
+const { Post, User, Comment, Budget } = require('../models');
 const withAuth = require('../utils/auth');
 
-
+// get all posts and budgets
 router.get('/', withAuth, (req, res) => {
-    Post.findAll({
+    Budget.findAll({
         where: {
             user_id: req.session.user_id
         },
-        attributes: ['id', 'story_title', 'story_body', 'created_at'],
+        attributes: ['id', 'budget_title'],
         include: [
-            {
-                model: Comment,
-                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-                include: {
-                    model: User,
-                    attributes: ['username']
-                }
-            },
             {
                 model: User,
                 attributes: ['username']
             }
         ]
     })
-    .then(dbPostData => {
-        const posts = dbPostData.map(post => post.get({ plain: true }));
-    
-        res.render('dashboard', {posts, loggedIn: true});
+    .then(dbBudgetData => {
+        const budgets = dbBudgetData.map(budget => budget.get({ plain: true }));
+        Post.findAll({
+            where: {
+                user_id: req.session.user_id
+            },
+            attributes: ['id', 'story_title', 'story_body', 'created_at'],
+            include: [
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        })
+        .then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+        
+            res.render('dashboard', {posts,budgets, loggedIn: true});
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
     })
     .catch(err => {
         console.log(err);
@@ -36,7 +55,7 @@ router.get('/', withAuth, (req, res) => {
 });
 
 
-// edit the post
+// edit the story
 router.get('/edit/:id', withAuth, (req, res) => {
     Post.findOne({
         where: {
@@ -73,10 +92,102 @@ router.get('/edit/:id', withAuth, (req, res) => {
         .catch(err => {
           res.status(500).json(err);
     });
+    Budget.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'budget_title'],
+        include: [
+            
+        ]
+    })
+    .then(dbBudgetData => {
+        if (dbBudgetData) {
+            const budget = dbBudgetData.get({ plain: true });
+            
+            res.render('edit-budget', {
+              budget,
+              loggedIn: true
+            });
+          } else {
+            res.status(404).end();
+          }
+        })
+        .catch(err => {
+          res.status(500).json(err);
+    });
 });
 
 // add story
 router.get('/addstory', withAuth, (req, res) => {
     res.render('add-story', {loggedIn: true});
+});
+
+// add budget
+router.get('/addbudget', withAuth, (req, res) => {
+    res.render('add-budget', {loggedIn: true});
+});
+
+// edit the budget
+// router.get('/edit/:id', withAuth, (req, res) => {
+//     Budget.findOne({
+//         where: {
+//             id: req.params.id
+//         },
+//         attributes: ['id', 'budget_title'],
+//         include: [
+            
+//         ]
+//     })
+//     .then(dbBudgetData => {
+//         if (dbBudgetData) {
+//             const budget = dbBudgetData.get({ plain: true });
+            
+//             res.render('edit-budget', {
+//               budget,
+//               loggedIn: true
+//             });
+//           } else {
+//             res.status(404).end();
+//           }
+//         })
+//         .catch(err => {
+//           res.status(500).json(err);
+//     });
+// });
+
+// get single budget
+router.get('/budget/:id', withAuth, (req, res) => {
+    Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: ['id', 'budget_title'],  
+      include: [
+                {
+          model: User,
+          attributes: ['id','username']
+        }
+      ]
+    })
+    .then(dbBudgetData => {
+      if (!dbBudgetData) {
+        res.status(404).json({ message: 'No budget found with this id '});
+        return;
+      }
+  
+      // serialize the data
+      const budget = dbBudgetData.get({ plain: true });
+  
+      // pass data to template
+      res.render('budget-view', {
+        budget,
+        loggedIn: req.session.loggedIn
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 module.exports = router;
