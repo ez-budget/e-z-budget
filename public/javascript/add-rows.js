c365572a836db8dab1425f2e87e72e8a9b3abee8
Kristen Groller <groller@rogers.com>
2021-03-28 11:33:02 -0400

    added rows to budget tables

--- public/javascript/add-rows.js
+++ public/javascript/add-rows.js
@@ -1,7 +1,6 @@
-const addRow =(function () {
+$(function () {
 
-    // Start counting from the third row
-    var counter = 3;
+    
 
     $("#insertRow").on("click", function (event) {
         event.preventDefault();
@@ -9,26 +8,47 @@ const addRow =(function () {
         var newRow = $("<tr>");
         var cols = '';
 
-        // Table columns
-        cols += '<th scope="row">' + counter + '</th>';
-        cols += '<td><input class="form-control rounded-0" type="text" name="income-source"></td>';
-        cols += '<td><input class="form-control rounded-0" type="text" name="income-receipt"></td>';
-        cols += '<td><input class="form-control rounded-0" type="text" name="income-remarks"></td>';
-        cols += '<td><button class="btn btn-danger rounded-0" id ="deleteRow"><i class="fa fa-trash"></i></button</td>';
+        // Income table columns
+        //cols += '<th scope="row">' + counter + '</th>';
+        cols += '<td><input type="text" class="form-control" id="income-source" name="income-source"/>';
+        cols += '<td><input type="text" class="form-control" id="income-receipt" name="income-receipt"/>';
+        cols += '<td><input type="text" class="form-control" id="income-remark" name="income-remark"/>';     
+
+
+
+        //cols += '<td><button class="btn btn-danger rounded-0" id ="deleteRow"><i class="fa fa-trash"></i></button</td>';
 
         // Insert the columns inside a row
         newRow.append(cols);
 
         // Insert the row inside a table
-        $("table").append(newRow);
-
-        // Increase counter after each row insertion
-        counter++;
+        $("#income-table").append(newRow);       
     });
+         
 
-    // Remove row when delete btn is clicked
-    $("table").on("click", "#deleteRow", function (event) {
-        $(this).closest("tr").remove();
-        counter -= 1
-    });
+    $("#insertRow2").on("click", function (event) {
+        event.preventDefault();
+
+        var newRow2 = $("<tr>");
+        var cols = '';
+
+        //Expense table columns
+        
+        cols += '<td><input type="text" class="form-control" id="expense-item" name="expense-item"/>';
+        cols += '<td><input type="text" class="form-control" id="expense-payment" name="expense-payment"/>';
+        cols += '<td><input type="text" class="form-control" id="expense-comment" name="expense-comment"/>';
+
+
+
+        //cols += '<td><button class="btn btn-danger rounded-0" id ="deleteRow"><i class="fa fa-trash"></i></button</td>';
+
+        // Insert the columns inside a row
+        newRow2.append(cols);
+
+        // Insert the row inside a table
+        $("#expense-table").append(newRow2);       
+    });  
+
+ 
 });
+
